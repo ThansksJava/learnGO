ca54b9421f04da98ba911176183867e410acfae6
fengjie <luckyjiefeng@163.com>
2019-03-11 19:51:07 +0800

    unnecessary ;

--- src/code/basic/main/hello.go
+++ src/code/basic/main/hello.go
@@ -1,7 +1,9 @@
 package main
+
 import "fmt"
-func main(){
-	fmt.Println("hello world!");
-	var vari = "fengjie";
-	fmt.Println(vari);
+
+func main() {
+	fmt.Println("hello world!")
+	var vari = "fengjie"
+	fmt.Println(vari)
 }
