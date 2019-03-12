13fbafcb38e240baaea2981787ff1d4e7ce1b88a
fengjie <luckyjiefeng@163.com>
2019-03-12 21:42:00 +0800

    interface practice

---
+++ src/code/basic/main/interfacetest.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+//接口中的函数也必须全被实现
+type query interface {
+	queryById(name string) string
+	//queryByName() string
+}
+
+//相当于一个实现类
+type queryImpl struct {
+}
+
+func (queryImpl) queryById(name string) string {
+	return name + "继承了接口query"
+}
+
+func main() {
+	//var qu  = queryImpl{}
+	var qu query = queryImpl{}
+	fmt.Println(qu.queryById("fengjie"))
+}
