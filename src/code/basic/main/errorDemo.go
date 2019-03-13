67a8dccbfbe2f011642f13ed37738edb3f500656
fengjie <luckyjiefeng@163.com>
2019-03-13 20:25:46 +0800

    error and channel practice

---
+++ src/code/basic/main/errorDemo.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func createError(num int) (int, error) {
+	if num < 0 {
+		return 0, errors.New("出现异常")
+	}
+	return num, nil
+}
+
+func main() {
+	result, err := createError(-1)
+	if err != nil {
+		fmt.Println(result)
+		fmt.Println(err)
+	}
+}
