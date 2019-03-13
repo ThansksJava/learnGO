9bbcc5269da43a3e0b36b841c0566a89db0d35d8
fengjie <luckyjiefeng@163.com>
2019-03-13 22:35:34 +0800

    implement sort Interface

---
+++ src/code/basic/main/sortDemo.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+)
+
+type humans struct {
+	name string
+	age  int
+}
+
+type black []humans
+
+func (af black) Len() int {
+	return len(af)
+}
+func (af black) Less(i, j int) bool {
+	return af[i].age > af[j].age
+}
+func (af black) Swap(i, j int) {
+	temp := af[i]
+	af[i] = af[j]
+	af[j] = temp
+}
+func main() {
+	var persons black
+	var i int
+	for i = 0; i < 10; i++ {
+		p := humans{
+			name: fmt.Sprintf("黑人%d", i),
+			age:  rand.Intn(100),
+		}
+		persons = append(persons, p)
+	}
+	fmt.Println("before sort")
+	fmt.Println(persons)
+	fmt.Println("after sort")
+	sort.Sort(persons)
+	fmt.Println(persons)
+
+}
