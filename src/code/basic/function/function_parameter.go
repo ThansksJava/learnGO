52e97a3ce302446e7d9d790ccd88682dfce7686f
fengjie <luckyjiefeng@163.com>
2019-07-28 21:01:38 +0800

    闭包实现斐波那契数列

--- src/code/basic/function/function_parameter.go
+++ src/code/basic/function/function_parameter.go
@@ -2,10 +2,12 @@ package function
 
 import "fmt"
 
+//ParamFunc 定义一个函数
 func ParamFunc(a int, b int) {
 	fmt.Printf("a=%d,b=%d", a, b)
 }
 
+//CallParmaFunc 调用闭包函数
 func CallParmaFunc(a int, b int, f func(int, int)) {
 	f(a, b)
 }
--- src/code/basic/main.go
+++ src/code/basic/main.go
@@ -1,8 +1,20 @@
 package main
 
-// import fmc "code/basic/function"
+import fmc "code/basic/function"
 // import pac "code/basic/practice"
+import "fmt"
 func main() {
 	// fmc.CallParmaFunc(1, 2, fmc.ParamFunc)
-	// pac.SayHello()
+	// pac.SayH ello()
+	// fmc.BiBao()
+
+	// retf := fmc.Add();
+	// fmt.Printf("2b=%d",retf(2))
+
+	fib := fmc.Fib()
+	for i := 0;i < 10;i++{
+		fmt.Printf("%d ",fib())
+	}
+
+
 }
