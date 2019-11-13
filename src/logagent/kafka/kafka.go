6d8bd8cf6526f5650551affb2dc032739edebcdd
fengjie <luckyjiefeng@163.com>
2019-11-13 09:22:00 +0800

    发送到kafka,漏提文件

---
+++ src/logagent/kafka/kafka.go
@@ -0,0 +1,42 @@
+package kafka
+
+import (
+	"fmt"
+	"github.com/Shopify/sarama"
+	"github.com/astaxie/beego/logs"
+)
+
+var (
+	client sarama.SyncProducer
+)
+
+//InitKafka 初始化kafka
+func InitKafka(addr string) (err error) {
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Partitioner = sarama.NewRandomPartitioner
+	config.Producer.Return.Successes = true
+	client, err = sarama.NewSyncProducer([]string{addr}, config)
+	if err != nil {
+		logs.Error("init kafka producer failed,err", err)
+		return
+	}
+	logs.Debug("init kafka success")
+	return
+}
+
+//SendToKafka 往kafka发送消息
+func SendToKafka(data, topic string) (err error) {
+	msg := &sarama.ProducerMessage{}
+	msg.Topic = topic
+	msg.Value = sarama.StringEncoder(data)
+	pid, offset, errSend := client.SendMessage(msg)
+	if errSend != nil {
+		fmt.Println("Send msg failed,", err)
+		logs.Error("Send msg failed,err :%v data:%v topic:%v", err, data, topic)
+		err = errSend
+		return
+	}
+	logs.Debug("pid:%v offset:%v\n", pid, offset)
+	return
+}
